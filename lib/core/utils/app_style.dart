1d69314b481e943df72c8369ef05e5b4463d0b09
tarek_elmarsafy <tarekelmarsafy50@gmail.com>
2025-05-12 10:49:44 +0300

    add AppStyle

---
+++ lib/core/utils/app_style.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/widgets.dart';
+
+class AppStyle {
+  static TextStyle regular12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Montserrat',
+  );
+
+  static TextStyle bold20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Montserrat',
+  );
+
+  static TextStyle semiBold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Montserrat',
+  );
+
+  static TextStyle semiBold18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Montserrat',
+  );
+}
