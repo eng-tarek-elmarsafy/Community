725a61c328fbeccf388317d9fc0e1ec3501ceb62
tarek_elmarsafy <tarekelmarsafy50@gmail.com>
2025-05-12 11:05:22 +0300

    add AppColor

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColor {
+  static const Color slider1 = Color(0xff667080);
+  static const Color y1 = Color(0xffFFAB01);
+  static const Color r1 = Color(0xffFE6250);
+  static const Color dr = Color(0xffB18CFE);
+  static const Color pk = Color(0xffFF8C82);
+  static const Color bul = Color(0xff82B4FF);
+  static const Color pg = Color(0xff64FCD9);
+  static Color wit = Color(0xffFFFFFF).withValues(alpha: .32);
+}
